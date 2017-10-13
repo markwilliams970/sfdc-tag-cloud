d992bb9783f686790642e94cc84e8641dc55bb3e
markwilliams970 <mtns842@gmail.com>
2017-10-12 18:31:09 -0600

    Split visualization into tag categories

--- extractor.js
+++ extractor.js
@@ -13,7 +13,19 @@ sfdc       = require('jsforce');
 dataDir    = 'web/data/';
 // Configuration and authentication information
 var config = require('./sfdcConfig.json');
+var topicCategories = require('./topicCategories.json');
 
+var productTopics       = topicCategories.product;
+var componentTopics     = topicCategories.component;
+var subComponentTopics  = topicCategories.subcomponent;
+var symptomTopics       = topicCategories.symptom;
+var causeTopics         = topicCategories.cause;
+
+const CATEGORY_PRODUCT        = "Product";
+const CATEGORY_COMPONENT      = "Component";
+const CATEGORY_SUBCOMPONENT   = "Sub-component";
+const CATEGORY_SYMPTOM        = "Symptom";
+const CATEGORY_CAUSE          = "Cause";
 
 getAllData = function () {
 
@@ -37,21 +49,60 @@ getAllData = function () {
 
         if (records.length > 0) {
 
-            var data = [];
+            var productData      = [];
+            var componentData    = [];
+            var subComponentData = [];
+            var symptomData      = [];
+            var causeData        = [];
+
             _.each(records, function(record) {
                 thisRecord = {
-                    "TopicName"   : record.Topic.Name,
-                    "TopicId"     : record.TopicId,
-                    "CreatedDate" : record.CreatedDate,
-                    "CreatedById" : record.CreatedById,
-                    "EntityType"  : record.EntityType,
-                    "EntityId"    : record.EntityId
+                    "TopicName"     : record.Topic.Name,
+                    "TopicId"       : record.TopicId,
+                    "TopicCategory" : "N/A",
+                    "CreatedDate"   : record.CreatedDate,
+                    "CreatedById"   : record.CreatedById,
+                    "EntityType"    : record.EntityType,
+                    "EntityId"      : record.EntityId,
+                };
+
+                if (productTopics.indexOf(record.Topic.Name) !== -1 ) {
+                    thisRecord.TopicCategory = CATEGORY_PRODUCT;
+                    productData.push(thisRecord);
                 }
-                data.push(thisRecord);
+                if (componentTopics.indexOf(record.Topic.Name) !== -1) {
+                    thisRecord.TopicCategory = CATEGORY_COMPONENT;
+                    componentData.push(thisRecord);
+                }
+                if (subComponentTopics.indexOf(record.Topic.Name) !== -1) {
+                    thisRecord.TopicCategory = CATEGORY_SUBCOMPONENT;
+                    subComponentData.push(thisRecord);
+                }
+                if (symptomTopics.indexOf(record.Topic.Name) !== -1) {
+                    thisRecord.TopicCategory = CATEGORY_SYMPTOM;
+                    symptomData.push(thisRecord);
+                }
+                if (causeTopics.indexOf(record.Topic.Name) !== -1) {
+                    thisRecord.TopicCategory = CATEGORY_CAUSE;
+                    causeData.push(thisRecord);
+                }
             });
             console.log("Found %s Topic Tags Applied", records.length);
-            console.log("Writing JSON data to %s.", dataDir + 'topics.json');
-            fs.writeFileSync(dataDir + 'topics.json', JSON.stringify(data, null, '\t'));
+
+            console.log("Writing Product JSON data to %s.", dataDir + 'product_topics.json');
+            fs.writeFileSync(dataDir + 'product_topics.json', JSON.stringify(productData, null, '\t'));
+
+            console.log("Writing Component JSON data to %s.", dataDir + 'component_topics.json');
+            fs.writeFileSync(dataDir + 'component_topics.json', JSON.stringify(componentData, null, '\t'));
+
+            console.log("Writing Sub-component JSON data to %s.", dataDir + 'subcomponent_topics.json');
+            fs.writeFileSync(dataDir + 'subcomponent_topics.json', JSON.stringify(subComponentData, null, '\t'));
+
+            console.log("Writing Symptom JSON data to %s.", dataDir + 'symptom_topics.json');
+            fs.writeFileSync(dataDir + 'symptom_topics.json', JSON.stringify(symptomData, null, '\t'));
+
+            console.log("Writing Cause JSON data to %s.", dataDir + 'cause_topics.json');
+            fs.writeFileSync(dataDir + 'cause_topics.json', JSON.stringify(causeData, null, '\t'));
         }
     }).on('error', function(err) {
         console.log(err);
--- web/app.js
+++ web/app.js
@@ -2,35 +2,86 @@ var app;
 
 app = angular.module('fdvis', ['ngRoute']);
 
-app.constant('fdVisualizations', [{
-  group: 'Case Data by Topic',
-  name: 'Top 25 Trending Topics',
-  route: '/totalByTopic',
-  href: '/vis/totalByTopic.html'
-}]);
+app.constant('fdVisualizations',
+    [
+        {
+          group: 'Case Data by Topic',
+          name: 'Topics By Product',
+          route: '/product',
+          href: '/vis/totalByProduct.html'
+        },
+        {
+            group: 'Case Data by Topic',
+            name: 'Topics By Component',
+            route: '/component',
+            href: '/vis/totalByComponent.html'
+        },
+        {
+            group: 'Case Data by Topic',
+            name: 'Topics By Sub-Component',
+            route: '/subcomponent',
+            href: '/vis/totalBySubComponent.html'
+        },
+        {
+            group: 'Case Data by Topic',
+            name: 'Topics By Symptom',
+            route: '/symptom',
+            href: '/vis/totalBySymptom.html'
+        },
+        {
+            group: 'Case Data by Topic',
+            name: 'Topics By Cause',
+            route: '/cause',
+            href: '/vis/totalByCause.html'
+        }
+    ]
+);
 
 app.service('fdVisualizationData', function ($http, $q) {
   var
-    messages,
-    topicsPromise,
-    users,
-    usersPromise,
+    products,
+    productsPromise,
+    components,
+    componentsPromise,
+    subcomponents,
+    subComponentsPromise,
+    symptoms,
+    symptomsPromise,
+    cause,
+    causePromise,
     promise,
     service;
 
-    topicsPromise = $http.get('/data/topics.json').success(function (data) { topics = data; return data; });
-  promise = $q.all({ topics: topicsPromise});
-  service = {};
+    productsPromise = $http.get('/data/product_topics.json').success(function (data) { products = data; return data; });
+    componentsPromise = $http.get('/data/component_topics.json').success(function (data) { components = data; return data; });
+    subComponentsPromise = $http.get('/data/subcomponent_topics.json').success(function (data) { subcomponents = data; return data; });
+    symptomsPromise = $http.get('/data/symptom_topics.json').success(function (data) { symptoms = data; return data; });
+    causePromise = $http.get('/data/cause_topics.json').success(function (data) { cause = data; return data; });
 
-  Object.defineProperties(service, {
-    promise : {
-      value: promise,
-      writable: false
-    },
-      topics: {
-      get: function () { return topics; }
-    }
-  });
+    promise = $q.all({ products: productsPromise, components: componentsPromise, subcomponents: subComponentsPromise, symptoms: symptomsPromise, cause: causePromise });
+    service = {};
+
+    Object.defineProperties(service, {
+        promise : {
+            value: promise,
+            writable: false
+        },
+        products: {
+            get: function () { return products; }
+        },
+        components: {
+            get: function () { return components; }
+        },
+        subcomponents: {
+            get: function () { return subcomponents; }
+        },
+        symptoms: {
+            get: function () { return symptoms; }
+        },
+        cause: {
+            get: function () { return cause; }
+        }
+    });
 
   return service;
 });
@@ -40,7 +91,9 @@ app.controller('fdNavBar', function fdNavBar($scope, fdVisualizations) {
 });
 
 app.config(function ($routeProvider, fdVisualizations) {
+    console.log(fdVisualizations);
   _.each(fdVisualizations, function (vis) {
+      console.log(vis);
     $routeProvider.when(vis.route, {
       resolve: {
         VisData: function (fdVisualizationData) { return fdVisualizationData.promise; }
